2ee68fe16baa50c0e20ac432ea0b3ce6ff8090b8
nicksdafish <97708899+nicksdafish@users.noreply.github.com>
2026-02-21 22:29:52 -0600

    Created a basic Movement script and added a cinemachine

--- Assets/Scripts/Test Scripts/PlayerMovement.cs
+++ Assets/Scripts/Test Scripts/PlayerMovement.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private InputActionAsset inputActions;
+
+    private InputAction moveAction;
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
+        rb.freezeRotation = true;
+
+        var map = inputActions.FindActionMap("Player", throwIfNotFound: true);
+        moveAction = map.FindAction("Move", throwIfNotFound: true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        moveAction.Enable(); 
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector2 dir = input.normalized;
+
+        rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
     }
 }
