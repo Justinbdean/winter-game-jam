918125946b9c35b5fcd64e604a1df31c7ca394a4
nicksdafish <97708899+nicksdafish@users.noreply.github.com>
2026-02-22 00:05:28 -0600

    added collision

--- Assets/Scripts/Test Scripts/PlayerMovement.cs
+++ Assets/Scripts/Test Scripts/PlayerMovement.cs
@@ -1,39 +1,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement3D : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private InputActionAsset inputActions;
 
     private InputAction moveAction;
-    private Rigidbody2D rb;
+    private Rigidbody rb;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0f;
+        rb = GetComponent<Rigidbody>();
+
+
+        rb.useGravity = false;
         rb.freezeRotation = true;
 
-        var map = inputActions.FindActionMap("Player", throwIfNotFound: true);
-        moveAction = map.FindAction("Move", throwIfNotFound: true);
+
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
+
+        var map = inputActions.FindActionMap("Player", true);
+        moveAction = map.FindAction("Move", true);
     }
 
-    private void OnEnable()
-    {
-        moveAction.Enable(); 
-    }
-
-    private void OnDisable()
-    {
-        moveAction.Disable();
-    }
+    private void OnEnable() => moveAction.Enable();
+    private void OnDisable() => moveAction.Disable();
 
     private void FixedUpdate()
     {
         Vector2 input = moveAction.ReadValue<Vector2>();
-        Vector2 dir = input.normalized;
 
-        rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
+        Vector3 move = new Vector3(input.x, input.y, 0f);
+
+        if (move.sqrMagnitude > 1f)
+            move.Normalize();
+
+        rb.linearVelocity = new Vector3(input.x, input.y, 0f) * moveSpeed * Time.deltaTime;
     }
 }
